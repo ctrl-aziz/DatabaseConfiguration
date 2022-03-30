ea2f2bd4c9e3d42f6afaa614cb0eea1af9a06b61
Aziz <alsmailabdalazez@gmail.com>
2022-03-30 12:23:55 +0300

    Add project files.

---
+++ DatabaseConfiguration/Database.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DatabaseConfiguration
+{
+    public class Database : Queries
+    {
+        private SqlConnection dbConnection;
+        private SqlCommand sql = null;
+
+        /// <summary>
+        /// When you want to insert or update data
+        /// you must use this construcot and pass [keys] 
+        /// and [values] parameters
+        /// </summary>
+        /// <param name="keys">this list will contain all fileds names that you want to update it or
+        /// add new data to database</param>
+        /// <param name="values">this list will contain all data that you want to update with or
+        /// add new data to database</param>
+        public Database(string databaseSource, string[] keys, string[] values) : base(keys, values)
+        {
+            dbConnection = new SqlConnection($"Data Source={databaseSource};Initial Catalog=DBLibrary;Integrated Security=True");
+        }
+
+        /// <summary>
+        /// Use this constructor if you only want to delete or get some data
+        /// from the database
+        /// </summary>
+        public Database(string databaseSource) : base()
+        {
+            dbConnection = new SqlConnection($"Data Source={databaseSource};Initial Catalog=DBLibrary;Integrated Security=True");
+        }
+        public int InsertData(string tableName)
+        {
+            SqlCommand sql = null;
+            try
+            {
+                dbConnection.Open();
+                sql = new SqlCommand(GetInsertQuery(tableName), dbConnection);
+                return sql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+
+        }
+
+        public int UpdateData(string tableName, string where, string isEqual)
+        {
+            SqlCommand sql = null;
+            try
+            {
+                dbConnection.Open();
+                sql = new SqlCommand(GetUpdateQuery(tableName, where, isEqual), dbConnection);
+                return sql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public int DeleteData(string tableName, string where, string isEqual)
+        {
+            SqlCommand sql = null;
+            try
+            {
+                dbConnection.Open();
+                sql = new SqlCommand(GetDeleteQuery(tableName, where, isEqual), dbConnection);
+                return sql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public SqlDataReader GetAllData(string tableName)
+        {
+            SqlCommand sql = null;
+            try
+            {
+                dbConnection.Open();
+                sql = new SqlCommand(GetAllDataQuery(tableName), dbConnection);
+                SqlDataReader _reader = sql.ExecuteReader();
+                
+                return _reader;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public SqlDataReader GetData(string tableName, string where, string isEqual)
+        {
+            SqlCommand sql = null;
+            try
+            {
+                dbConnection.Open();
+                sql = new SqlCommand(GetDataQuery(tableName, where, isEqual), dbConnection);
+                SqlDataReader _reader = sql.ExecuteReader();
+                return _reader;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (dbConnection.State == System.Data.ConnectionState.Open)
+            {
+                dbConnection.Close();
+                if (sql != null)
+                {
+                    sql.Dispose();
+                }
+            }
+        }
+
+
+        
+    }
+}
---
+++ DatabaseConfiguration/Queries.cs
@@ -0,0 +1,55 @@
+﻿namespace DatabaseConfiguration
+{
+    public class Queries
+    {
+        public string[] keys = {};
+        public string[] values = {};
+
+        public Queries(string[] keys, string[] values)
+        {
+            this.keys = keys;
+            this.values = values;
+        }
+
+        public Queries()
+        {
+        }
+
+        public string GetInsertQuery(string tableName)
+        {
+            /*
+            string withAt = string.Join(",@", keys);
+            string valuesName = "@" + withAt;
+            */
+            return $"insert into {tableName} ({string.Join(",", keys)}) values({string.Join(",", values)});";
+        }
+
+        public string GetUpdateQuery(string tableName, string where, string isEqual)
+        {
+            string updateText = "";
+            for (int i = 0; i < keys.Length; i++)
+            {
+                updateText += $"{keys[i]}={values[i]}";
+            }
+            return $"Update {tableName} set {updateText} where {where}={isEqual};";
+        }
+
+        public string GetDeleteQuery(string tableName, string where, string isEqual)
+        {
+            return $"Delete from {tableName} where {where}={isEqual}";
+        }
+
+
+        public string GetDataQuery(string tableName, string where, string isEqual)
+        {
+            return $"Select * from {tableName} where {where}={isEqual}";
+        }
+
+        public string GetAllDataQuery(string tableName)
+        {
+            return $"Select * from {tableName}";
+        }
+
+
+    }
+}
